cc53f2103a1374bc2dc5a2d83f143c15b26f70b7
Kamahin Mihail <kamahinmihail@gmail.com>
2020-08-13 21:50:16 +0300

    1

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,49 @@
-module.exports.getAll = function(req, res) {
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
+module.exports.getAll = async function(req, res) {
+    try {
+        const categories = await new Category.find({user: req.user.id});
+        res.status(200).json(categories);
+    } catch(e) {
+        errorHandler(res, e);
+    }
 };
 
-module.exports.getById = function(req, res) {
-    
+module.exports.getById = async function(req, res) {
+    try {
+        const categories = await new Category.findById(req.params.id);
+        res.status(200).json(categories);
+    } catch(e) {
+        errorHandler(res, e);
+    }
 };
 
 module.exports.remove = function(req, res) {
-    
+    try {
+        await new Category.remove({_id: req.params.id});
+        await new Position.remove({category: req.params.id});
+        res.status(200).json({
+            message: 'Категория удалена'
+        });
+    } catch(e) {
+        errorHandler(res, e);
+    }
 };
 
 module.exports.create = function(req, res) {
-    
+    try {
+
+    } catch(e) {
+        errorHandler(res, e);
+    }
 };
 
 module.exports.update = function(req, res) {
-    
+    try {
+
+    } catch(e) {
+        errorHandler(res, e);
+    }
 };
