0e26473c4c9af1025c1d1d7ac1e2d03a65a32aa9
Kamahin Mihail <kamahinmihail@gmail.com>
2020-08-27 22:40:16 +0300

    pipe add

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/internal/operators';
 
 import { User } from '../interfaces';
 
@@ -25,7 +26,7 @@ export class AuthService {
           localStorage.setItem('auth-token', token);
           this.setToken(token);
         })
-      )
+      );
   }
 
   setToken(token: string) {
