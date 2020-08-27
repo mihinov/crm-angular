1c1ead8c23211c7cf39a8dd906127e05ce48b77e
Kamahin Mihail <kamahinmihail@gmail.com>
2020-08-27 22:38:22 +0300

    auth service

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,26 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
+  aSub: Subscription;
 
-  constructor() { }
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+      password: new FormControl(null, [Validators.required, Validators.minLength(2)])
+    });
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        // Теперь вы можете зайти в систему используя свои данные
+      } else if (params['accessDenied']) {
+        // Для начала авторизуйтесь в системе
+      }
     });
   }
 
-  onSubmit() {
+  ngOnDestroy() {
+    if (this.aSub) {
+      this.aSub.unsubscribe();
+    }
+  }
 
+  onSubmit() {
+    this.form.disable();
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => {
+        console.log('login sucess');
+        this.router.navigate(['/overview']);
+      },
+      err => console.warn(err)
+    );
   }
 
 }
