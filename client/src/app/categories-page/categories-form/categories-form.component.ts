5a0d0182ebdec921f67e3832f6bf60895186f980
Kamahin Mihail <kamahinmihail@gmail.com>
2020-09-05 17:49:10 +0300

    categories

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CategoriesService } from '../../shared/services/categories.service';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { MaterialService } from '../../shared/services/material.service';
 
 @Component({
   selector: 'app-categories-form',
@@ -12,18 +16,42 @@ export class CategoriesFormComponent implements OnInit {
   isNew = true;
   form: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private categoriesService: CategoriesService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       name: new FormControl(null, Validators.required)
     });
 
-    this.route.params.subscribe((params: Params) => {
-      if (params.id) {
-        this.isNew = false;
-      }
-    });
+    this.form.disable();
+
+    this.route.params
+      .pipe(
+        switchMap(
+          (params: Params) => {
+            if (params.id) {
+              this.isNew = false;
+              return this.categoriesService.getById(params.id);
+            } else {
+              return of(null);
+            }
+          }
+        )
+      )
+      .subscribe(
+        category => {
+          if (category) {
+            this.form.patchValue({
+              name: category.name
+            });
+            MaterialService.updateTextInputs();
+          }
+
+          this.form.enable();
+        },
+        err => MaterialService.toast(err.error.message)
+      );
   }
 
   onSubmit(): void {
